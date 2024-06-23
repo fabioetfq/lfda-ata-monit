7784bfdb3cd74ea498f4047052536e30fc0d151d
Fabio Oliveira <fabioetfq@gmail.com>
2024-06-23 19:54:35 -0300

    pregao routs, firts pregao actions and methods, new pregao view

--- app/controllers/pregaos_controller.rb
+++ app/controllers/pregaos_controller.rb
@@ -1,9 +1,26 @@
 class PregaosController < ApplicationController
+  before_action :set_pregao
+  def new
+    @pregao = Pregao.new
+  end
+
+  def create
+    @pregao = Pregao.new(pregao_params)
+    if @pregao.save
+      redirect_to pregao_path(@pregao)
+    else
+      redirect_to new_pregao_path
+    end
+  end
 
   private
 
   def pregao_params
     params.require(:pregao).permit(:num_pregao)
+  end
+
+  def set_pregao
+    @pregao = Pregao.find(params[:id])
   end
 
 end
