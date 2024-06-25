ec1603299583f9da3c36193a993e83d0e47e127f
Fabio Oliveira <fabioetfq@gmail.com>
2024-06-25 19:49:23 -0300

    CRUD de Suppliers. Precisa acertar o destroy

--- app/controllers/suppliers_controller.rb
+++ app/controllers/suppliers_controller.rb
@@ -1,2 +1,46 @@
 class SuppliersController < ApplicationController
+  before_action :set_supplier, only: %i[show edit update destroy]
+
+  def index
+    @suppliers = Supplier.all
+  end
+
+  def show
+  end
+
+  def new
+    @supplier = Supplier.new
+  end
+
+  def create
+    @supplier = Supplier.new(supplier_params)
+    if @supplier.save
+      redirect_to supplier_path(@supplier)
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @supplier.update(supplier_params)
+    redirect_to supplier_path(@supplier)
+  end
+
+  def destroy
+    @supplier.destroy
+    redirect_to suppliers_path, status: :see_other
+  end
+
+  private
+
+  def supplier_params
+    params.require(:supplier).permit(:name, :email, :cnpj)
+  end
+
+  def set_supplier
+    @supplier = Supplier.find(params[:id])
+  end
 end
