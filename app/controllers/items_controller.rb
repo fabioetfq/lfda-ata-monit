1372482dc9543fb9f9deef17e851e1c2649a5dc6
Fabio Oliveira <fabioetfq@gmail.com>
2024-06-26 11:11:35 -0300

    Index and show Item actions

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,15 @@
 class ItemsController < ApplicationController
+  before_action only: %i[show]
+  def index
+    @items = Item.all
+  end
+
+  def show
+  end
+
+  private
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
 end
