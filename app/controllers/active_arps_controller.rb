bfa26fc510e5582c54332016478f5b86db3e3d68
Fabio Oliveira <fabioetfq@gmail.com>
2024-08-05 17:54:50 -0300

    index, show, new and create ok

--- app/controllers/active_arps_controller.rb
+++ app/controllers/active_arps_controller.rb
@@ -1,2 +1,31 @@
 class ActiveArpsController < ApplicationController
+  before_action :set_active_arp, only: %i[show]
+  def index
+    @active_arps = ActiveArp.all
+  end
+
+  def show; end
+
+  def new
+    @active_arp = ActiveArp.new
+  end
+
+  def create
+    @active_arp = ActiveArp.new(active_arp_params)
+    if @active_arp.save
+      redirect_to active_arp_path(@active_arp)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def active_arp_params
+    params.require(:active_arp).permit(:arp_id, :item_id, :item_value, :unit, :qty_item, :date_start, :validity, :institution_id)
+  end
+
+  def set_active_arp
+    @active_arp = ActiveArp.find(params[:id])
+  end
 end
