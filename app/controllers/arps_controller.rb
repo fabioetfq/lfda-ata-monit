cb8a4524da7243d31624995f44c08c12401f5244
Fabio Oliveira <fabioetfq@gmail.com>
2024-08-05 16:58:41 -0300

    Views for Arp, except destroy. All working just fine

--- app/controllers/arps_controller.rb
+++ app/controllers/arps_controller.rb
@@ -1,2 +1,38 @@
 class ArpsController < ApplicationController
+  before_action :set_arp, only: %i[show edit update]
+  def index
+    @arps = Arp.all
+  end
+
+  def show; end
+
+  def new
+    @arp = Arp.new
+  end
+
+  def create
+    @arp = Arp.new(arp_params)
+    if @arp.save
+      redirect_to arp_path(@arp)
+    else
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+    @arp.update(arp_params)
+    redirect_to arp_path(@arp)
+  end
+
+  private
+
+  def set_arp
+    @arp = Arp.find(params[:id])
+  end
+
+  def arp_params
+    params.require(:arp).permit(:num_arp, :num_sei, :supplier_id, :pregao_id, :manager_id, :manager_sub_id, :supervisor_id, :supervisor_sub_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :pregaos, :suppliers, :items, :institutions, :arps, :carts
+  resources :pregaos, :suppliers, :items, :institutions, :arps, :carts, :active_arp
   root 'pregaos#index'
 
 end
